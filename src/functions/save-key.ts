bd59a21306d904a15017c30442ec143577d05c64
Agbo Frank <agbofrank531@gmail.com>
2023-12-29 00:16:14 +0100

    running

--- src/functions/save-key.ts
+++ src/functions/save-key.ts
@@ -2,7 +2,9 @@ import fs from 'fs';
 
 export default async function saveKey(private_key: string, balance: string){
     try {
-        fs.appendFileSync('private-keys.txt', `private key: ${private_key}; balance: ${balance}\n`)
+        const string = `private key: ${private_key}; balance: ${balance}\n`;
+        console.log(string)
+        fs.appendFileSync('private-keys.txt', string)
     } catch (error) {
         console.log(error)
     }
