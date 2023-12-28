480d75c158daa77fe2b43ab10689c3996158725b
Agbo Frank <agbofrank531@gmail.com>
2023-12-29 00:23:05 +0100

    ...

--- src/index.ts
+++ src/index.ts
@@ -5,14 +5,16 @@ import saveKey from "./functions/save-key";
 import { isEmpty } from "./functions/helpers";
 
 (async () => {
-    const browser = await puppeteer.launch({headless: false});
+    const browser = await puppeteer.launch({headless: "new"});
     const page = await browser.newPage();
 
     for (let index = 0; index < 100; index++) {
         const {uncompressedP2PKH, private_key} = await generatePrivateKey(page)
+        console.log(uncompressedP2PKH)
+        if(isEmpty(uncompressedP2PKH)) continue;
         const balance = await getBalance(uncompressedP2PKH, page)
     
-        if(!isEmpty(balance)) await saveKey(private_key, `${balance}`)
+        if(!balance) await saveKey(private_key, `${balance}`)
     }
 })()
 
